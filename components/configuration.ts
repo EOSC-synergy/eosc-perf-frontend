3d0be60761497129612cc2ab4d16e0470f635eb2
Christophe <christophe.misc+git@protonmail.ch>
2022-04-16 04:35:56 +0200

    Move API host + route to .env

--- components/configuration.ts
+++ components/configuration.ts
@@ -1,7 +1,4 @@
-export const API_HOST = '';
-export const API_ROUTE = '/api';
-export const API_VERSION = '/v1';
+export const API_HOST = process.env.NEXT_PUBLIC_API_HOST ?? "";
+export const API_ROUTE = process.env.NEXT_PUBLIC_API_ROUTE ?? "";
 
-export const API_BASE_ROUTE = API_ROUTE + API_VERSION;
-
-export const API_BASE_PATH = API_HOST + API_BASE_ROUTE;
+export const API_BASE_PATH = API_HOST + API_ROUTE;
