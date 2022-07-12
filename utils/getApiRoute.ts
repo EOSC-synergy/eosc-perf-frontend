fe901454470132728d1bda6e7d4954b7b28f3e7b
Christophe <christophe.misc+git@protonmail.ch>
2022-07-12 12:17:25 +0200

    fix: Default to production API on production build

--- utils/getApiRoute.ts
+++ utils/getApiRoute.ts
@@ -2,7 +2,7 @@ const DEV_API_HOST = 'https://perf.test.fedcloud.eu/api/v1';
 const PROD_API_HOST = 'https://performance.services.fedcloud.eu/api/v1';
 
 const getApiRoute = (env = process.env.NEXT_PUBLIC_API_ROUTE): string => {
-    let host = env ?? DEV_API_HOST;
+    let host = env ?? (process.env.NODE_ENV === 'development' ? DEV_API_HOST : PROD_API_HOST);
 
     if (env === 'production') {
         host = PROD_API_HOST;
