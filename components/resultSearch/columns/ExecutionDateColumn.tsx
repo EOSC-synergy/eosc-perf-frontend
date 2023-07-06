d6be06347d57efa1eca1f914de88e4fd31538f99
Christophe <christophe.misc+git@protonmail.ch>
2023-07-06 14:20:40 +0200

    fix(search): use formatted execution dates

--- components/resultSearch/columns/ExecutionDateColumn.tsx
+++ components/resultSearch/columns/ExecutionDateColumn.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import { Result } from '@eosc-perf/eosc-perf-client';
+import { formatDistance } from 'date-fns';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 type ExecutionDateColumnProps = {
     result: Result;
@@ -9,8 +11,32 @@ type ExecutionDateColumnProps = {
  * Column to display the time the result was obtained
  * @param result
  */
-const ExecutionDateColumn: FC<ExecutionDateColumnProps> = ({ result }) => (
-    <>{result.execution_datetime}</>
-);
+const ExecutionDateColumn: FC<ExecutionDateColumnProps> = ({ result }) => {
+    const executionDate = new Date(result.execution_datetime);
+
+    return (
+        <span
+            style={{
+                textDecoration: 'underline',
+                textDecorationStyle: 'dotted',
+                textDecorationColor: 'rgba(0, 0, 0, 0.33)',
+            }}
+        >
+            <OverlayTrigger
+                overlay={
+                    <Tooltip id={result.id + '-tooltip'}>
+                        {executionDate.toLocaleString(undefined)}
+                    </Tooltip>
+                }
+            >
+                <span>
+                    {formatDistance(executionDate, new Date(), {
+                        addSuffix: true,
+                    })}
+                </span>
+            </OverlayTrigger>
+        </span>
+    );
+};
 
 export default ExecutionDateColumn;
