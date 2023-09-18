1fa2c6bfe183cff7aa2dc797711b14d8c6b7a62b
Christophe <christophe.misc+git@protonmail.ch>
2023-09-18 18:21:22 +0200

    fix(UnselectedTag): use semantic button

--- components/TagSelector/UnselectedTag.tsx
+++ components/TagSelector/UnselectedTag.tsx
@@ -1,17 +1,13 @@
 import { type FC } from 'react';
 import styles from 'styles/tagSelector.module.css';
-import actionable from 'styles/actionable.module.css';
 import { type Tag } from '@eosc-perf/eosc-perf-client';
 import clsx from 'clsx';
 
 type UnselectedTagProps = { tag: Tag; select: (tag: Tag) => void };
 const UnselectedTag: FC<UnselectedTagProps> = ({ tag, select }) => (
-    <div
-        className={clsx(styles['tagBadge'], 'p-1', actionable['actionable'])}
-        onClick={() => select(tag)}
-    >
+    <button className={clsx(styles['tagBadge'], 'p-1')} onClick={() => select(tag)} type="button">
         {tag.name}
-    </div>
+    </button>
 );
 
 export default UnselectedTag;
